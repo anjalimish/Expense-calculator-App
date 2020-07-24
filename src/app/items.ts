1fceb374f24a45c77060a36b08dbcdcd9d1c86cf
anjalimish <anjalimish6@gmail.com>
2020-07-24 08:59:57 +0530

    first commit

---
+++ src/app/items.ts
@@ -0,0 +1,8 @@
+export class Item {
+  amount: number;
+  name: string;
+  date: string;
+  name1: string;
+  balance: number;
+  status: string;
+}
---
+++ src/app/mock-items.ts
@@ -0,0 +1,8 @@
+import { Item } from './items';
+
+
+export const ITEMS: Item[] = [
+  { amount: 10, name: 'canteen', date: '2017-07-20', name1:'anjali(me)', balance:2 , status: 'Pending'},
+  { amount: 15, name: 'Fanfest ', date: '2017-07-21', name1:'amisha', balance:12 , status: "Clear"  },
+
+];
